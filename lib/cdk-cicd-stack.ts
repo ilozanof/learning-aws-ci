70d21b0b4482bd1d08e0fd1cb4837177a46f9f98
ilozanof <i.fernandez@nchain.com>
2024-07-31 11:19:35 +0200

    pipeline init

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('ilozanof/learning-aws-ci', 'main'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ]
+      })
+    })
   }
 }
